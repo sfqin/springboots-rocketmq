2d63f1f0a9d6e2c8f9f519eb76d029a594a07d62
qinzhe <sfqincsu@163.com>
2020-04-04 19:01:07 +0800

    init

---
+++ rocketmq-producer/src/main/java/com/example/rocketmq/rocketmqproducer/RocketmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.rocketmq.rocketmqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqProducerApplication.class, args);
+    }
+
+}
